45054b1ba1cf6b070aa19ab5cc2087f3ed548646
kkatwo <kkatwo@gmail.com>
2014-03-15 18:03:37 +0900

    プロジェクトの追加

---
+++ Probability verification/Probability verification/MainWindow.Designer.cs
@@ -0,0 +1,223 @@
+﻿namespace Probability_verification
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.executeButton = new System.Windows.Forms.Button();
+            this.resultTextBox = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.crystalCount = new System.Windows.Forms.NumericUpDown();
+            this.resetThreshold = new System.Windows.Forms.NumericUpDown();
+            this.setCount = new System.Windows.Forms.NumericUpDown();
+            this.halfResetCheck = new System.Windows.Forms.CheckBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.halfResetThreshold = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.crystalCount)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.resetThreshold)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.setCount)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.halfResetThreshold)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // executeButton
+            // 
+            this.executeButton.Location = new System.Drawing.Point(201, 242);
+            this.executeButton.Name = "executeButton";
+            this.executeButton.Size = new System.Drawing.Size(90, 47);
+            this.executeButton.TabIndex = 0;
+            this.executeButton.Text = "実行";
+            this.executeButton.UseVisualStyleBackColor = true;
+            this.executeButton.Click += new System.EventHandler(this.executeButton_Click);
+            // 
+            // resultTextBox
+            // 
+            this.resultTextBox.Location = new System.Drawing.Point(297, 12);
+            this.resultTextBox.Multiline = true;
+            this.resultTextBox.Name = "resultTextBox";
+            this.resultTextBox.ReadOnly = true;
+            this.resultTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.resultTextBox.Size = new System.Drawing.Size(250, 277);
+            this.resultTextBox.TabIndex = 1;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.crystalCount);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(137, 48);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "クリスタル個数";
+            // 
+            // crystalCount
+            // 
+            this.crystalCount.Location = new System.Drawing.Point(6, 18);
+            this.crystalCount.Maximum = new decimal(new int[] {
+            100000,
+            0,
+            0,
+            0});
+            this.crystalCount.Name = "crystalCount";
+            this.crystalCount.Size = new System.Drawing.Size(120, 19);
+            this.crystalCount.TabIndex = 0;
+            this.crystalCount.Value = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            // 
+            // resetThreshold
+            // 
+            this.resetThreshold.Location = new System.Drawing.Point(132, 123);
+            this.resetThreshold.Maximum = new decimal(new int[] {
+            99,
+            0,
+            0,
+            0});
+            this.resetThreshold.Name = "resetThreshold";
+            this.resetThreshold.Size = new System.Drawing.Size(120, 19);
+            this.resetThreshold.TabIndex = 3;
+            // 
+            // setCount
+            // 
+            this.setCount.Location = new System.Drawing.Point(17, 18);
+            this.setCount.Maximum = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            0});
+            this.setCount.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.setCount.Name = "setCount";
+            this.setCount.Size = new System.Drawing.Size(120, 19);
+            this.setCount.TabIndex = 4;
+            this.setCount.Value = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            // 
+            // halfResetCheck
+            // 
+            this.halfResetCheck.AutoSize = true;
+            this.halfResetCheck.Location = new System.Drawing.Point(12, 101);
+            this.halfResetCheck.Name = "halfResetCheck";
+            this.halfResetCheck.Size = new System.Drawing.Size(104, 16);
+            this.halfResetCheck.TabIndex = 5;
+            this.halfResetCheck.Text = "中間リセット判定";
+            this.halfResetCheck.UseVisualStyleBackColor = true;
+            this.halfResetCheck.CheckedChanged += new System.EventHandler(this.halfResetCheck_CheckedChanged);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.setCount);
+            this.groupBox2.Location = new System.Drawing.Point(12, 242);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(156, 47);
+            this.groupBox2.TabIndex = 6;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "試行回数";
+            // 
+            // halfResetThreshold
+            // 
+            this.halfResetThreshold.Location = new System.Drawing.Point(132, 98);
+            this.halfResetThreshold.Maximum = new decimal(new int[] {
+            99,
+            0,
+            0,
+            0});
+            this.halfResetThreshold.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.halfResetThreshold.Name = "halfResetThreshold";
+            this.halfResetThreshold.Size = new System.Drawing.Size(120, 19);
+            this.halfResetThreshold.TabIndex = 7;
+            this.halfResetThreshold.Value = new decimal(new int[] {
+            50,
+            0,
+            0,
+            0});
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 130);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(97, 12);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "無条件リセット閾値";
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(562, 301);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.halfResetThreshold);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.halfResetCheck);
+            this.Controls.Add(this.resetThreshold);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.resultTextBox);
+            this.Controls.Add(this.executeButton);
+            this.Name = "MainWindow";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.MainWindow_Load);
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.crystalCount)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.resetThreshold)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.setCount)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.halfResetThreshold)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button executeButton;
+        private System.Windows.Forms.TextBox resultTextBox;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.NumericUpDown crystalCount;
+        private System.Windows.Forms.NumericUpDown resetThreshold;
+        private System.Windows.Forms.NumericUpDown setCount;
+        private System.Windows.Forms.CheckBox halfResetCheck;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.NumericUpDown halfResetThreshold;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Probability verification/Probability verification/MainWindow.cs
@@ -0,0 +1,226 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Probability_verification
+{
+    /// <summary>
+    /// メインウィンドウ
+    /// </summary>
+    public partial class MainWindow : Form
+    {
+        /// <summary>
+        /// アイテムデータ
+        /// </summary>
+        public class ItemData
+        {
+            public string Type { get; set; }
+
+            /// <summary>
+            /// コンストラクタ
+            /// </summary>
+            /// <param name="type"></param>
+            public ItemData(string type)
+            {
+                Type = type;
+            }
+        }
+
+        private const string ITEMTYPE_OTHER = "other";
+        private const string ITEMTYPE_TARGET = "target";
+        private const string ITEMTYPE_RESET = "reset";
+        private Random rnd = new Random();
+
+
+        private List<ItemData> restItems = new List<ItemData>();
+        private Dictionary<string,int> getItems = new Dictionary<string, int>();
+
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void UpdateHalfResetThresholdEnabled()
+        {
+            halfResetThreshold.Enabled = halfResetCheck.Checked;
+        }
+
+        /// <summary>
+        /// アイテムデータのリセット
+        /// </summary>
+        private void ResetItemData()
+        {
+            restItems.Clear();
+
+            //  リセット可能アイテム
+            restItems.Add(new ItemData(ITEMTYPE_RESET));
+            //  目的アイテム
+            restItems.Add(new ItemData(ITEMTYPE_TARGET));
+            restItems.Add(new ItemData(ITEMTYPE_TARGET));
+            //  埋める
+            while( restItems.Count < 100 ) {
+                restItems.Add(new ItemData(ITEMTYPE_OTHER));
+            }
+        }
+        /// <summary>
+        /// リセット対象アイテム取得済みか
+        /// </summary>
+        /// <returns></returns>
+        private bool IsGotResetItem()
+        {
+            foreach( ItemData item in restItems)
+            {
+                if (item.Type == ITEMTYPE_RESET)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        private int RestTargetItemCount()
+        {
+            int count = 0;
+            foreach (ItemData item in restItems)
+            {
+                if (item.Type == ITEMTYPE_TARGET)
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+        /// <summary>
+        /// 実効ボタンクリック
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void executeButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                executeButton.Enabled = false;
+
+                //  入手済みアイテムのリセット
+                getItems.Clear();
+
+                for (int i = 0; i < (int)setCount.Value; i++)
+                {
+                    ExecuteOneSet();
+                }
+
+                //  平均化
+                Dictionary<string, double> result = new Dictionary<string, double>();
+                foreach (KeyValuePair<string, int> pair in getItems)
+                {
+                    //  平均化
+                    result[pair.Key] = (double)getItems[pair.Key] / (int)setCount.Value;
+                    //  確率化
+                    result[pair.Key] = result[pair.Key] / (int)crystalCount.Value * 100;
+                }
+
+                //  書き出し
+                string resultText = "---- " + DateTime.Now + " ----" + Environment.NewLine;
+                resultText += "無条件残閾値:" + resetThreshold.Value  + Environment.NewLine;
+                if (halfResetCheck.Checked)
+                {
+                    resultText += "中間残閾値:" + halfResetThreshold.Value + Environment.NewLine;
+                }
+                foreach (KeyValuePair<string, double> pair in result)
+                {
+                    resultText += pair.Key + " : " + pair.Value + "%" + Environment.NewLine;
+                }
+                resultTextBox.Text = resultText + resultTextBox.Text;
+
+            }
+            catch (Exception ex)
+            {
+            }
+            finally
+            {
+                executeButton.Enabled = true;
+            }
+        }
+
+        private void ExecuteOneSet()
+        {
+
+            //  最初のデータリセット
+            ResetItemData();
+
+            for (int i = 0; i < (int)crystalCount.Value; i++)
+            {
+                bool isReset = false;
+                int itemNum = rnd.Next() % restItems.Count;
+                string type = restItems[itemNum].Type;
+                restItems.RemoveAt(itemNum);
+
+                try
+                {
+                    getItems[type] = getItems[type] + 1;
+                }
+                catch (KeyNotFoundException ex)
+                {
+                    getItems[type] = 1;
+                }
+                         
+                //  アイテム切れでリセット
+                if (restItems.Count <= 0)
+                {
+                    isReset = true;
+                }
+
+                //  目的達成でリセット
+                if (RestTargetItemCount() == 0)
+                {
+                    isReset = true;
+                }
+
+                //  目的アイテムちょっと取得済み
+                if (halfResetCheck.Checked)
+                {
+                    //  1個取得済みで残りアイテム数が一定以上の場合はリセット
+                    if (RestTargetItemCount() == 1 &&
+                        (int)halfResetThreshold.Value < restItems.Count)
+                    {
+                        isReset = true;
+                    }
+                }
+
+                //  閾値超える
+                if ( restItems.Count <= (int)resetThreshold.Value )
+                {
+                    isReset = true;
+                }
+
+                //  アイテムのリセット
+                if( isReset && IsGotResetItem() )
+                {
+                    ResetItemData();
+                }
+            }
+        }
+
+        private void MainWindow_Load(object sender, EventArgs e)
+        {
+            UpdateHalfResetThresholdEnabled();
+        }
+
+        private void halfResetCheck_CheckedChanged(object sender, EventArgs e)
+        {
+            UpdateHalfResetThresholdEnabled();
+        }
+    }
+}
